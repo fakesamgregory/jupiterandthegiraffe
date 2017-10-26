1d87908b855dbaa0b542bf172a15a0690ca6b44d
Sam Gregory <samuel@digital-detox.co.uk>
2017-10-26 19:41:55 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ fi
 #build
 npm run build -- --prod
 
-mv -t dist src/share-image.jpg src/.htaccess src/google7cfd879982ead749.html
+mv -t dist src/share-image.jpg src/.htaccess src/google7cfd879982ead749.html src/.htaccess
 
 tar -zcvf $TARGET.tar.gz dist
 

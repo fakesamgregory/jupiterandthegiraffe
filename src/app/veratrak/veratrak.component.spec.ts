9d216132a6675a1201aa052a2f568bc8fa35b33b
Samuel Gregory <samuelgregory@me.com>
2018-08-26 11:41:07 +0100

    typo

--- src/app/veratrak/veratrak.component.spec.ts
+++ src/app/veratrak/veratrak.component.spec.ts
@@ -3,7 +3,7 @@ import { async, TestBed } from '@angular/core/testing';
 import { VeratrakComponent } from './veratrak.component';
 
 describe('VeratrakComponent', () => {
-  let COMPONENT: VeratrakComponent;
+  const COMPONENT: VeratrakComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({

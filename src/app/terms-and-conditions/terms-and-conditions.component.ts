74334b4237356dd8b84c21039bf149d50aeacfc7
Sam Gregory <samuel@digital-detox.co.uk>
2018-09-16 19:14:57 +0100

    remove onInit

--- src/app/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-terms-and-conditions',
   templateUrl: './terms-and-conditions.component.html',
   styleUrls: ['./terms-and-conditions.component.scss']
 })
-export class TermsAndConditionsComponent implements OnInit {
+export class TermsAndConditionsComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }

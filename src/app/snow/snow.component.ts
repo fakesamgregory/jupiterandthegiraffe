b53619f8244434eb9ec9252b0d9d16be5190595a
Samuel Gregory <fakesamgregory@users.noreply.github.com>
2018-12-16 20:00:27 +0000

    add snow!! (#38)

---
+++ src/app/snow/snow.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snow-component',
+  templateUrl: './snow.component.html',
+  styleUrls: ['./snow.component.scss']
+})
+export class SnowComponent { }

fd3a3f4e6b2070c5034b45945ec3b52b195c2510
Samuel Gregory <samuelgregory@me.com>
2019-01-16 21:38:22 +0000

    change to utc string

--- src/app/cookie-banner/cookie-banner.component.ts
+++ src/app/cookie-banner/cookie-banner.component.ts
@@ -24,7 +24,7 @@ export class CookieBannerComponent implements OnInit {
   setCookie(name, value, days) {
     const d = new Date;
     d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
-    document.cookie = `${name}=${value};path=/;expires=${d.toGMTString()}`;
+    document.cookie = `${name}=${value};path=/;expires=${d.toUTCString()}`;
   }
 
   dismiss() {

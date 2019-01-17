4775430aa608f1bef5ad1c258fcb6d9340816b78
Samuel Gregory <samuelgregory@me.com>
2019-01-17 16:10:46 +0000

    add cms integration

--- src/app/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-terms-and-conditions',
   templateUrl: './terms-and-conditions.component.html',
   styleUrls: ['./terms-and-conditions.component.scss']
 })
-export class TermsAndConditionsComponent {
-  constructor(private meta: Meta, private titleService: Title) {
+export class TermsAndConditionsComponent implements OnInit {
+  private url = 'https://blog.jupiterandthegiraffe.com/wp-json/wp/v2';
+  public content = '';
+  public title = '';
+
+  constructor(private meta: Meta, private titleService: Title, private http: HttpClient) {
     const TITLE = 'Terms and Conditions';
     const DESC = 'Check out our T&C\'s';
 
@@ -29,5 +34,14 @@ export class TermsAndConditionsComponent {
       itemprop: 'name',
       content: TITLE
     });
+  }
+
+  ngOnInit() {
+    this.http
+      .get(`${this.url}/pages/144`)
+      .subscribe((blogs: Array<any>) => {
+        this.content = blogs.content.rendered;
+        this.title = blogs.title.rendered;
+        });
   }
 }

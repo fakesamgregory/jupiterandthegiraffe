c60dec199ece29d2b4a5ffcb0bbd2158082ba45f
Samuel Gregory <samuelgregory@me.com>
2019-03-07 12:15:31 +1100

    add timer

--- src/app/pages/home/friends/friends.component.ts
+++ src/app/pages/home/friends/friends.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,9 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.scss']
 })
-export class FriendsComponent implements OnInit {
+export class FriendsComponent implements OnInit, AfterViewInit {
   public friends: Array<Object>;
   private postsUrl = 'assets/json/friends.json';
+  private currectActiveSlide = 0;
+  @ViewChild('slider', {read: ElementRef}) slider: ElementRef;
 
   constructor(private http: HttpClient) {}
 
@@ -18,5 +20,45 @@ export class FriendsComponent implements OnInit {
       .subscribe((data: Object) => {
         this.friends = data['friends'];
       });
+  }
+
+  ngAfterViewInit(): void {
+    this.setupSlider();
+  }
+
+  setupSlider(): void {
+    const slides = Array.prototype.slice.call(this.slider.nativeElement.children);
+    const slideTimerSecs = 6;
+
+    this.slider.nativeElement.style.height =
+      slides.reduce((accumulator, currentValue) => {
+        const height = currentValue.offsetHeight;
+        currentValue.style.position = 'absolute';
+
+        if (height > accumulator) {
+          return height;
+        } else {
+          return accumulator;
+        }
+      }, 0) + 'px';
+
+    slides[0].classList.add('slide--visible');
+
+    setInterval(() => {
+      slides.forEach((slide, index) => {
+        slide.classList.remove('slide--visible');
+        slide.setAttribute('aria-hidden', true);
+
+        if (index === this.currectActiveSlide) {
+          slide.classList.add('slide--visible');
+          slide.setAttribute('aria-hidden', false);
+        }
+      });
+
+      this.currectActiveSlide +=  1;
+      if (this.currectActiveSlide > slides.length - 1) {
+        this.currectActiveSlide = 0;
+      }
+    }, slideTimerSecs * 1000);
   }
 }

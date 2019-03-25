8fb7917f19188d5f2ba6bd7f4363410b033a667e
Samuel Gregory <samuelgregory@me.com>
2019-03-25 11:23:28 +1100

    add stuff

--- src/app/pages/what/what.component.ts
+++ src/app/pages/what/what.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, Inject, PLATFORM_ID, ViewChild} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
+import {WINDOW} from '@ng-toolkit/universal';
 
 @Component({
   templateUrl: './what.component.html',
   styleUrls: ['./what.component.scss']
 })
 export class WhatComponent {
-  constructor(private meta: Meta, private titleService: Title) {
+  public fixSidebar = false;
+  public fixedCSS: object;
+  @ViewChild('fixedWrap') fixedWrap: ElementRef;
+
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+    @Inject(PLATFORM_ID) private platformId: any,
+    @Inject(WINDOW) private window: Window,
+    @Inject(DOCUMENT) private document: Document
+  ) {
     const TITLE = 'What We Do - Branding, Websites, UX/UI, Logos';
     const DESC = 'We care about your brand as much as you do, we have experience in logo design and corporate identity.' +
       'To finish it off, we can build you a kickass website to go with it.';
@@ -29,5 +41,18 @@ export class WhatComponent {
       itemprop: 'name',
       content: TITLE
     });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      const scrollY = this.window.pageYOffset || this.document.documentElement.scrollTop;
+      console.log(scrollY, this.fixedWrap.nativeElement.getBoundingClientRect().top);
+      this.fixSidebar = (scrollY > this.fixedWrap.nativeElement.getBoundingClientRect().top);
+      this.fixedCSS = this.fixSidebar ? {
+        top: 0,
+        left: `${this.fixedWrap.nativeElement.getBoundingClientRect().left}px`,
+      } : {};
+    }
   }
 }

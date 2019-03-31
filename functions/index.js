dffe5dfc7c6f553f78a573af2f62c84e4ea9294e
Samuel Gregory <samuelgregory@me.com>
2019-03-31 14:30:19 +1100

    email sender and reciever

--- functions/index.js
+++ functions/index.js
@@ -31,14 +31,31 @@ exports.sendContactMessage = functions.database
       return null;
     }
 
-    return mailTransport.sendMail({
-        from: 'Jupiter and the Giraffe <salam@jupiterandthegiraffe.com>',
-        to: ['samuel@jupiterandthegiraffe.com', 'suzannah@jupiterandthegiraffe.com'],
-        replyTo: val.email,
-        subject: `${val.name} contacted Jupiter and the Giraffe`,
-        html: val.html,
-        text: val.message
-      })
-      .then(error => error ? console.log(error) : console.log('Mail sent to: samuel@jupiterandthegiraffe.com'))
+    const mailToUs = mailTransport.sendMail({
+      from: 'Jupiter and the Giraffe <salam@jupiterandthegiraffe.com>',
+      to: ['samuel@jupiterandthegiraffe.com', 'suzannah@jupiterandthegiraffe.com'],
+      replyTo: val.email,
+      subject: `${val.name} contacted Jupiter and the Giraffe`,
+      html: val.html,
+      text: val.message
+    });
+
+    const html = '<p>Hey, ' + val.name.split(' ')[0] + '!</p>' +
+      '<p>Thanks for contacting Jupiter and the Giraffe. We know how busy life can be so we appreciate ' +
+      'the time you\'ve spent visiting our website.</p>' +
+      '<p>Rest-assured we\'ve recieved your message loud and clear and we\'ll get back to you as soon as we can.</p>' +
+      '<p>Have an awesome day,</p>' +
+      '<p>The Giraffe</p>';
+
+    const mailToThem = mailTransport.sendMail({
+      from: 'Jupiter and the Giraffe <noreply@jupiterandthegiraffe.com>',
+      to: val.email,
+      subject: "🚀 Thanks for contacting Jupiter and the Giraffe, you're awesome!",
+      html,
+      text: html.replace(/<\/p>/g, ' ').replace(/<p>/g, '')
+    });
+
+    return Promise.all([mailToUs, mailToThem])
+      .then(error => error ? console.log(error) : console.log('Mail sent!'))
       .catch(e => console.log(e));
   });

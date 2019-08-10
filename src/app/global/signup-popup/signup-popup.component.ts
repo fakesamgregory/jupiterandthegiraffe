9fc6854b7582553065f276b1e9cc6e317f84a436
Samuel Gregory <samuelgregory@me.com>
2019-08-10 13:20:13 +0800

    add cookies

--- src/app/global/signup-popup/signup-popup.component.ts
+++ src/app/global/signup-popup/signup-popup.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { WINDOW } from '@ng-toolkit/universal';
+import { DOCUMENT } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-signup-popup',
@@ -9,13 +11,18 @@ import { WINDOW } from '@ng-toolkit/universal';
 export class SignupPopupComponent implements OnInit {
   public showDialog = false;
   public showPrompt = false;
+  private cookieName = 'emailAcknowledged';
+  private cookieTimeInSeconds = 10;
 
-  constructor(@Inject(WINDOW) private window: Window) {
-
-  }
+  constructor(
+    @Inject(WINDOW) private window: Window,
+    @Inject(DOCUMENT) private document: Window,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
 
   public closePrompt() {
     this.showPrompt = false;
+    this.setCookie(this.cookieName, true, 365);
   }
 
   public toggleDialog(e) {
@@ -27,9 +34,24 @@ export class SignupPopupComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.window.setTimeout(() => {
-      this.showPrompt = true;
-    }, 5000)
+    if (isPlatformBrowser(this.platformId)) {
+      if (!this.getCookie(this.cookieName)) {
+        this.window.setTimeout(() => {
+          this.showPrompt = true;
+        }, this.cookieTimeInSeconds * 1000)
+      }
+    }
+  }
+
+  getCookie(name) {
+    const v = this.document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
+    return v ? v[2] : null;
+  }
+
+  setCookie(name, value, days) {
+    const d = new Date;
+    d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+    this.document.cookie = `${name}=${value};path=/;expires=${d.toUTCString()}`;
   }
 
 }

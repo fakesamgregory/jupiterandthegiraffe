ba0d7b58a74b7a0ffe2632333ef5748e592e23fc
Samuel Gregory <fakesamgregory@users.noreply.github.com>
2019-08-27 14:16:33 +0800

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -189,7 +189,7 @@ export class HomeComponent {
     @Inject(PLATFORM_ID) private platformId: any
   ) {
 
-    const TITLE = 'Branding and Design for Future-Thinking Tech Startups | 🚀 Jupiter and the Giraffe<';
+    const TITLE = 'Branding and Design for Future-Thinking Tech Startups | 🚀 Jupiter and the Giraffe';
     const DESC = 'Helping future-thinking tech startups grow and stand out in the market because revolutionary technology ' +
       'needs revolutionary design. Contact us now!';
 

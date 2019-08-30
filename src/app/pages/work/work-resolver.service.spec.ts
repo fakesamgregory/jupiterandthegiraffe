521b833fc86c82f4043f42699d2fc73db6eb9876
Samuel Gregory <samuelgregory@me.com>
2019-08-30 08:02:10 +0800

    fix error

--- src/app/pages/work/work-resolver.service.spec.ts
+++ src/app/pages/work/work-resolver.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { WorkResolverService } from './work-resolver.service';
-import {DataService} from '../../data.service';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterTestingModule} from '@angular/router/testing';
 
@@ -9,7 +8,7 @@ describe('WorkResolverService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, RouterTestingModule],
-      providers: [WorkResolverService, DataService]
+      providers: [WorkResolverService]
     });
   });
 

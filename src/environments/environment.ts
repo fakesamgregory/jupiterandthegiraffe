efb9c334cd1e07a21d43e05584c1ffafa32ddaff
Samuel Gregory <samuelgregory@me.com>
2019-09-15 11:25:06 +0800

    hide api keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyA0He8885a4_wSLc4ZKkzT4-DW3gNfeg-E',
+    apiKey: 'FIREBASE_MAPS_API_KEY',
     authDomain: 'jupiterandthegiraffe-1662d.firebaseapp.com',
     databaseURL: 'https://jupiterandthegiraffe-1662d.firebaseio.com',
     projectId: 'jupiterandthegiraffe-1662d',

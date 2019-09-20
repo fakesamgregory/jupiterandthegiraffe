3dc05040994fcfd52d53d86599af7065c51a6f48
Samuel Gregory <fakesamgregory@users.noreply.github.com>
2019-09-20 14:06:54 +0800

    Feature/add ooo (#104)
    * add page
    
    * minor styles

---
+++ src/app/global/climate-strike/climate-strike.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-climate-strike',
+  templateUrl: './climate-strike.component.html',
+  styleUrls: ['./climate-strike.component.scss']
+})
+export class ClimateStrikeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

f45223355fce074ab0a90abcce389d6f0d1075fc
Samuel Gregory <samuelgregory@me.com>
2019-09-23 08:40:06 +0800

    fix linting

--- src/app/directives/video-load.directive.ts
+++ src/app/directives/video-load.directive.ts
@@ -6,14 +6,17 @@ import { Directive, Input, ElementRef, HostListener, Renderer, Renderer2 } from 
 export class VideoLoadDirective {
   @Input('videoId') id = '';
   constructor(
-    private _element: ElementRef,
+    private elementRef: ElementRef,
     private renderer: Renderer2
     ) { }
-    
-  @HostListener('click') loadVideo(){
-    this.renderer.addClass(this._element.nativeElement, 'video--playing');
-    this._element.nativeElement.innerHTML = 
-      `<iframe src="https://www.youtube.com/embed/${this.id}?rel=0&showinfo=0&autoplay=1" frameborder="0" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>`;
+
+  @HostListener('click') loadVideo (){
+    this.renderer.addClass(this.elementRef.nativeElement, 'video--playing');
+    this.elementRef.nativeElement.innerHTML =
+      `<iframe 
+        src="https://www.youtube.com/embed/${this.id}?rel=0&showinfo=0&autoplay=1" 
+        frameborder="0" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen
+        ></iframe>`;
 
   }
 }
